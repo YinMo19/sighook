e3444b17dd6312cb62db0be1e6b87471f77392e7
YinMo19 <me@yinmo19.top>
2026-02-07 21:10:25 +0800

    ci: run on all branches and add example smoke runs; add linux init_array

--- examples/instrument_no_original/main.rs
+++ examples/instrument_no_original/main.rs
@@ -10,6 +10,7 @@ extern "C" fn replace_logic(_address: u64, ctx: *mut HookContext) {
 
 #[used]
 #[cfg_attr(target_os = "macos", unsafe(link_section = "__DATA,__mod_init_func"))]
+#[cfg_attr(target_os = "linux", unsafe(link_section = ".init_array"))]
 static INIT_ARRAY: extern "C" fn() = init;
 
 extern "C" fn init() {
